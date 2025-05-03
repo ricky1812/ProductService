5d8a49f1370857bf1f448a63fd6db12154614466
ricky1812 <rickydas189@gmail.com>
2025-05-03 14:27:38 +0530

    Added get all products API for 3rd Party

---
+++ src/main/java/com/example/productservice/controllerAdvice/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.productservice.controllerAdvice;
+
+import com.example.productservice.dto.ProductNotFondDTo;
+import com.example.productservice.exceptions.ProductNotFoundException;
+import com.example.productservice.models.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(ProductNotFoundException.class)
+  public ResponseEntity<ProductNotFondDTo> handleProductNotFoundException(ProductNotFoundException e) {
+    ProductNotFondDTo productNotFondDTo = new ProductNotFondDTo();
+    productNotFondDTo.setMessage(e.getMessage()+" "+e.getProductID());
+    productNotFondDTo.setResolution("Ensure proper id is given");
+    return new ResponseEntity<>(
+        productNotFondDTo
+        ,HttpStatus.NOT_FOUND);
+  }
+
+
+}
---
+++ src/main/java/com/example/productservice/dto/ProductNotFondDTo.java
@@ -0,0 +1,12 @@
+package com.example.productservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductNotFondDTo {
+  private String message;
+  private String resolution;
+
+}
