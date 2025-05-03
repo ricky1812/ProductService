22dffa2ab42c28f1b6ca5736afd914374522f610
ricky1812 <rickydas189@gmail.com>
2025-05-03 15:49:27 +0530

    Added connections to mysql db

--- src/main/java/com/example/productservice/models/Category.java
+++ src/main/java/com/example/productservice/models/Category.java
@@ -1,10 +1,12 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name="category")
 public class Category extends BaseModel {
   private String value;
 
--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,15 +1,19 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name="products")
 public class Product extends BaseModel {
   private Long id;
   private String title;
   private String description;
   private double price;
+  @ManyToOne
   private Category category;
   private String image;
 
