bba980ab42265c9045959ba8ba67e4b6ea455ab9
ricky1812 <rickydas189@gmail.com>
2025-05-19 17:05:22 +0530

    added date insertion during update and creation

--- src/main/java/com/example/productservice/models/BaseModel.java
+++ src/main/java/com/example/productservice/models/BaseModel.java
@@ -1,22 +1,33 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseModel {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseModel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Temporal(TemporalType.TIMESTAMP)
+  @CreatedDate
   private Date createdAt;
+  @Temporal(TemporalType.TIMESTAMP)
+  @LastModifiedDate
   private Date updatedAt;
 
 }
